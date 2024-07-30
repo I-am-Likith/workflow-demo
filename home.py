6aa05f8c1271308e2aa46a54d9cb3184df5e069f
Likith <panasalikith@gmail.com>
2024-07-30 13:10:47 +0530

    modified

--- home.py
+++ home.py
@@ -1,2 +1,15 @@
-def selection():
-  pass
+def sel(lst):
+    l = len(lst)
+    for i in range(l):
+        min = i
+        for j in range(i+1,l):
+            if lst[j] < lst[min]:
+                min = j
+        temp = lst[i]
+        lst[i] = lst[min]
+        lst[min] = temp
+    return lst
+
+lst = [13,46,54,22,20,11]
+res= sel(lst)
+print(res)
